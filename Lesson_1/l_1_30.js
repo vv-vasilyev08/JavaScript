80d2312bd0fe8d8ff5d91515f1a2d88db1ee1d1d
vv-vasilyev08 <qa.happyit08@gmail.com>
2022-06-18 14:33:04 +0300

    add Lesson_1

---
+++ Lesson_1/l_1_30.js
@@ -0,0 +1,48 @@
+let user_name = "Vadim"
+let name2 = "Vadim"
+
+// console.log(name2)
+
+
+let age = 32
+// console.log(age)
+
+// let person = user_name + " " + age
+// console.log(person)
+
+// console.log(user_name + "______" + age)
+
+let user_age = 30
+let work_period = 10
+
+// let total_time = user_age + work_period
+// console.log("total_time = " + total_time)
+// console.log("total_time = ", total_time, typeof(total_time))
+// console.log("total_time = " + total_time, typeof("total_time = " + total_time))
+
+// let total_time = user_age - work_period
+// console.log("total_time = ", total_time, typeof(total_time))
+
+// let total_time = user_age * work_period
+// console.log("total_time = ", total_time, typeof(total_time))
+
+let morning = false
+console.log("morning = ", morning, typeof(morning))
+
+let evening = true
+console.log("evening = ", evening, typeof(evening))
+
+let apple = 15
+let cherry = 5
+
+let compare = apple >= cherry
+console.log("compare = ", compare)
+
+if (compare){
+    console.log("Compare is", compare)
+} else if (apple == 15){
+    console.log("apple == ", apple)
+}
+ else{
+    console.log("!!!Compare is", false)
+}
